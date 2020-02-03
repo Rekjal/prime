059c682bad1ec67146c3df7612eaa90b1e2c3102
Rekjal <32828154+Rekjal@users.noreply.github.com>
2020-02-02 20:07:54 -0800

    Copies from Desktop. Great work

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  $('#formOne').submit(function(){
+    let input = $('#userString').val(); // do not remove
+    //console.log(`input:: ${input}`);
+    var  numbers = [];
+    let keyNumbers = [];
+    var primeArray = [];
+
+    for(var k = 2; k <= input; k++ ){
+      numbers.push(k);
+    } 
+
+    keyNumbers.push(2);
+    numbers.forEach(function(number) {
+      if(number % 2 !== 0){
+        keyNumbers.push(number);
+      }
+    });
+
+   //console.log(`Line:16:KeyNumbers:numbers at start:: ${keyNumbers} : ${numbers}`);
+   let spliceArray = [];
+
+   for(let i = 0; i < keyNumbers.length; i++) {
+     for (let j = 0; j < numbers.length; j++) {
+          if((numbers[j] % keyNumbers[i] === 0) && (numbers[j] !== keyNumbers[i]) && ($.inArray(numbers[j], spliceArray) === -1) ) {
+            spliceArray.push(numbers[j]);
+          }
+      }
+    }   
+    for (var m = 0; m< numbers.length; m++) {
+      if($.inArray(numbers[m], spliceArray) === -1){
+        primeArray.push(numbers[m]);
+      }
+    }
+    // console.log(`Key Numbers at end: ${keyNumbers}`);
+    // console.log(`Numbers at end: ${numbers}`);
+    // console.log(`spliceArray at end: ${spliceArray}`);
+    console.log(`primeArray at end: ${primeArray}`);
+
+    $("#limit").empty().append(input);
+    $("#story").show();
+    $("#puzzle").empty().prepend(primeArray.join(", "));
+
+    event.preventDefault(); 
+  });
+});
